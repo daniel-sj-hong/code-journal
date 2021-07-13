3db7391d3f2aa158bb373a94d6d93a43eccfdeee
Daniel Hong <dan8492@gmail.com>
2021-07-12 17:22:09 -0700

    queried the form and added an event listener for submitting. working on call back function now

--- js/main.js
+++ js/main.js
@@ -2,9 +2,15 @@
 /* exported data */
 var $photo = document.querySelector('#photo-url');
 var $img = document.querySelector('img');
+var $form = document.querySelector('form');
 
 $photo.addEventListener('input', handleInput);
+$form.addEventListener('submit', handleSubmit);
 
 function handleInput(event) {
   $img.setAttribute('src', event.target.value);
+}
+
+function handleSubmit(event) {
+
 }
